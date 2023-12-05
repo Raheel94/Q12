92c78ac34d999779b0f6fe6224cca24e231995b4
Raheel94 <muha.raheel94@gmail.com>
2023-12-05 13:36:26 +0500

    go

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name
+//, print a message to them. The text of each message should be the same, 
+//but each message should be personalized with the person’s name.
+// declaring an array variable to store names
+let names = ['Raheel', 'Amman', 'Zainub', 'Usman'];
+// To print
+console.log("You are invited to the wedding,", names[0] + ".");
+console.log("You are invited to the wedding,", names[1] + ".");
+console.log("You are invited to the wedding,", names[2] + ".");
+console.log("You are invited to the wedding,", names[3] + ".");
---
+++ index.ts
@@ -0,0 +1,14 @@
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name
+//, print a message to them. The text of each message should be the same, 
+//but each message should be personalized with the person’s name.
+
+// declaring an array variable to store names
+
+let names: string [] = ['Raheel','Amman','Zainub','Usman']
+
+// To print
+
+console.log("You are invited to the wedding,",names[0]+".")
+console.log("You are invited to the wedding,",names[1]+".")
+console.log("You are invited to the wedding,",names[2]+".")
+console.log("You are invited to the wedding,",names[3]+".")
